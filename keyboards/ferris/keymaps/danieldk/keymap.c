01da4f382194c7fc3bde22e8fd77ac3300adc9a8
Daniël de Kok <me@danieldk.eu>
2022-10-26 17:31:37 +0200

    Move vim-like movement keys to the right
    This also moves the mouse layer to the left, since it is activated
    by layer key on the right.

--- keyboards/ferris/keymaps/danieldk/keymap.c
+++ keyboards/ferris/keymaps/danieldk/keymap.c
@@ -10,19 +10,19 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
         KC_Q, KC_W, KC_J, KC_R, KC_T, KC_Y,                        KC_U, KC_I, KC_O, KC_P,
-        LSFT_T(KC_A), LT(5,KC_S), LT(1,KC_D), LT(3,KC_F), KC_G,    KC_H, LT(4,KC_N), LT(2,KC_E), LT(6,KC_L), LSFT_T(KC_SCLN),
+        LSFT_T(KC_A), LT(5,KC_S), LT(2,KC_D), LT(3,KC_F), KC_G,    KC_H, LT(4,KC_N), LT(1,KC_E), LT(6,KC_L), LSFT_T(KC_SCLN),
         KC_Z, LCTL_T(KC_X), LALT_T(KC_C), KC_V, KC_B,              KC_K, KC_M, LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
         MT(MOD_LCTL,KC_TAB), KC_BSPC,                              LT(7,KC_SPC), MT(MOD_RGUI,KC_ENT)),
 	[1] = LAYOUT_split_3x5_2(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_BTN1, KC_WH_U, KC_BTN2, KC_TRNS,
-        KC_TRNS, KC_BTN2, KC_NO, KC_BTN1, KC_TRNS,                 KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_R, KC_TRNS,
+        KC_TRNS, KC_BTN1, KC_WH_U, KC_BTN2, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,               KC_TRNS, KC_BTN2, KC_NO, KC_BTN1, KC_TRNS,
+        KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_R, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,                                          KC_TRNS, KC_TRNS
     ),
 	[2] = LAYOUT_split_3x5_2(
-        KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS,                 KC_TRNS, KC_LGUI, KC_NO, LCTL(KC_LALT), LCA(KC_LSFT),
-        KC_TRNS, KC_HOME, KC_PGDN, KC_END, KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_LGUI, KC_NO, LCTL(KC_LALT), LCA(KC_LSFT),      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_HOME, KC_PGDN, KC_END, KC_TRNS,
         KC_TRNS, KC_TRNS,                                          KC_TRNS, KC_TRNS
     ),
 	[3] = LAYOUT_split_3x5_2(
