300dc3c2a8473dd10c693bea262f2869984b9abe
Daniël de Kok <me@danieldk.eu>
2022-10-28 17:38:31 +0200

    Tamak 2

--- keyboards/ferris/keymaps/danieldk/keymap.c
+++ keyboards/ferris/keymaps/danieldk/keymap.c
@@ -9,8 +9,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
-        KC_Q, KC_W, KC_J, KC_R, KC_T, KC_Y,                        KC_U, KC_I, KC_O, KC_P,
-        LSFT_T(KC_A), LT(5,KC_S), LT(2,KC_D), LT(3,KC_F), KC_G,    KC_H, LT(4,KC_N), LT(1,KC_E), LT(6,KC_L), LSFT_T(KC_SCLN),
+        KC_Q, KC_W, KC_F, KC_R, KC_G, KC_Y,                        KC_U, KC_I, KC_O, KC_P,
+        LSFT_T(KC_A), LT(5,KC_S), LT(2,KC_D), LT(3,KC_T), KC_J,    KC_H, LT(4,KC_N), LT(1,KC_E), LT(6,KC_L), LSFT_T(KC_SCLN),
         KC_Z, LCTL_T(KC_X), LALT_T(KC_C), KC_V, KC_B,              KC_K, KC_M, LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
         MT(MOD_LCTL,KC_TAB), KC_BSPC,                              LT(7,KC_SPC), MT(MOD_RGUI,KC_ENT)),
 	[1] = LAYOUT_split_3x5_2(
