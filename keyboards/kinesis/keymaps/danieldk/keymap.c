6a781387ddd49925cd06908916aed36d8b4217ff
Daniël de Kok <me@danieldk.eu>
2022-12-17 10:49:36 +0100

    kinesis/danieldk: Mac thumb cluster, capslock as escape

--- keyboards/kinesis/keymaps/danieldk/keymap.c
+++ keyboards/kinesis/keymaps/danieldk/keymap.c
@@ -9,10 +9,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_ESC, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            KC_TAB, KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,
-           KC_CAPS,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,
+           KC_ESC, KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,
            KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,
-                   KC_GRV ,KC_INS ,KC_LEFT,KC_RGHT,
-			   KC_LCTL,KC_LALT,
+                   KC_GRV ,KC_CAPS,KC_LEFT,KC_RGHT,
+			   KC_LGUI,KC_LALT,
                                     KC_HOME,
                            KC_BSPC,KC_DEL ,KC_END ,
     KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SCRL  ,KC_PAUS, KC_NO, QK_BOOT,
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_M   ,KC_N   ,KC_E   ,KC_I   ,KC_O,   KC_QUOT,
 	KC_K   ,KC_H   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
 		KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
-           KC_RGUI,KC_RCTL,
+           KC_LCTL,KC_RGUI,
            KC_PGUP,
            KC_PGDN,KC_ENTER ,KC_SPC
     )
