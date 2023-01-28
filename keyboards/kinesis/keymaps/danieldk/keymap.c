80f1651f28f24d996b4ae6372a222c48abb6a9d6
Daniël de Kok <me@danieldk.eu>
2023-01-28 13:24:56 +0100

    kinesis/danieldk: put command mod-tap on middle fingers

--- keyboards/kinesis/keymaps/danieldk/keymap.c
+++ keyboards/kinesis/keymaps/danieldk/keymap.c
@@ -12,6 +12,8 @@ enum layer_names {
 // Home row mods
 uint16_t const _T_SFT = SFT_T(KC_T);  // Shift on indexes
 uint16_t const _N_SFT = SFT_T(KC_N);
+uint16_t const _S_CMD = CMD_T(KC_S);  // Command on middle
+uint16_t const _E_CMD = CMD_T(KC_E);
 
 // MacOS shortcuts
 uint16_t const _1PASS = LGUI(KC_BSLS);        // 1Password
@@ -26,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,
     KC_EQL, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,
     KC_TAB, KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,
-    KC_ESC, KC_A,   KC_R,   KC_S,   _T_SFT, KC_G,
+    KC_ESC, KC_A,   KC_R,   _S_CMD, _T_SFT, KC_G,
     KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,
             KC_GRV, CW_TOGG,KC_LEFT,KC_RGHT,
                     KC_LGUI,KC_LALT,
@@ -36,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F9,  KC_F10, KC_F11, KC_F12, _SCRNS, KC_VOLD,  KC_VOLU, KC_WAKE, QK_BOOT,
     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
     KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,KC_BSLS,
-    KC_M,   _N_SFT, KC_E,   KC_I,   KC_O,   KC_QUOT,
+    KC_M,   _N_SFT, _E_CMD, KC_I,   KC_O,   KC_QUOT,
     KC_K,   KC_H,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
             KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,
                     KC_LCTL,KC_RGUI,
