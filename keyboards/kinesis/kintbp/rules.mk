8f8455fb9ae41876297c1f139d14505f5cf715d0
Daniël de Kok <me@danieldk.eu>
2023-06-03 13:28:50 +0200

    kinesis/kintbp: bootloader address is already in the MCU definition

--- keyboards/kinesis/kintbp/rules.mk
+++ keyboards/kinesis/kintbp/rules.mk
@@ -5,9 +5,6 @@ BOARD = BLACKPILL_STM32_F411
 # Bootloader selection
 BOOTLOADER = stm32-dfu
 
-# Address of the bootloader in system memory
-STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
-
 # Build Options
 #   change yes to no to disable
 #
