745667fe5ca02c6e689e067b20ae748df234d5c9
niels <niels.heinen@gmail.com>
2023-06-18 22:16:07 +0200

    Fix bool return

--- keyboards/kinesis/keymaps/danieldk/keymap.c
+++ keyboards/kinesis/keymaps/danieldk/keymap.c
@@ -79,8 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    if (!process_achordion(keycode, record)) { return false; }
-    return true;
+    return process_achordion(keycode, record);
 }
 
 void matrix_scan_user(void) {
